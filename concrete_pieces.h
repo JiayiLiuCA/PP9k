beb53d4eb5fda21f2208e071cd35d7224e1f63a8
zihanchen <zihanchen.ca@gmail.com>
2015-07-18 17:43:19 -0400

    update

--- concrete_pieces.h
+++ concrete_pieces.h
@@ -2,38 +2,44 @@
 #define __CONCRETE_PIECES_H__
 	
 #include "pieces.h"
+#include <vector>
 
 class King:public Pieces {
-		vector <Pieces*> attackers;
+		std::vector <Pieces*> attackers;
 	public:
-		King(int r, int c, char name,vector <Pieces*> attackers): row(r), col(c), name(name), attackers(attackers);
-		moveCheck();
+		King(int r, int c, char name, std::vector <Pieces*> attackers): Pieces(), row(r), col(c), name(name), attackers(attackers) {}
+		bool moveCheck();
 };
 
 class Queen:public Pieces {
 	public:
-		moveCheck();
+		Queen(int r, int c, char name): Pieces(), row(r), col(c), name(name) {}
+		bool moveCheck();
 };
 
 class Bishop:public Pieces {
 	public:
-		moveCheck();
+		Bishop(int r, int c, char name): Pieces(), row(r), col(c), name(name) {}
+		bool moveCheck();
 };
 
 class Rook:public Pieces {
 	public:
-		moveCheck();
+		Rook(int r, int c, char name): Pieces(), row(r), col(c), name(name) {}
+		bool moveCheck();
 };
 
 class Knight:public Pieces {
 	public:
-		moveCheck();
+		Knight(int r, int c, char name): Pieces(), row(r), col(c), name(name) {}
+		bool moveCheck();
 };
 
 class Pawn:public Pieces {
 		bool doubleMove; //if a Pawn has just moved 2 cells then this is true
 	public:
-		moveCheck();
+		Pawn(int r, int c, char name, bool move): Pieces(), row(r), col(c), name(name), doubleMove(move) {}
+		bool moveCheck();
 		bool getStatus(); //a getter for doubleMove
 };
 #endif
--- pieces.h
+++ pieces.h
@@ -2,10 +2,10 @@
 
 
 class Pieces {
+	protected:
 		char name;
 		int row;
 		int col;
 	public:
-		bool moveCheck(int oldr, int oldc, int newr, int newc) = 0; //this check whether the move made by
-							  //the piece is valid or not on a plane board
+		virtual bool moveCheck(int oldr, int oldc, int newr, int newc) = 0; //this check whether the move made by the piece is valid or not on a plane board
 };
