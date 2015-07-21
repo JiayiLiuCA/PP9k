d2790517a42d83f5e778c756b5c258d0b3f12c84
j437liu <j437liu@>
2015-07-21 15:43:37 -0400

    aldskfj;lasdfha;lkfh;lawsekhf;oweqihfpoewarjhsogh

--- King.cc
+++ King.cc
@@ -10,7 +10,15 @@ bool King::moveCheck(int row, int col, int new_row, int new_col) {
 	int diff_row = std::abs(row - new_row);
 	int diff_col = std::abs(col - new_col);
 	if (getStatus() == false && diff_row == 0 && diff_col == 2) {
-		return true;
+		if (getName() == 'P' && row == 7 && col == 4) {
+			return true;
+		}
+		else if (getName() == 'p' && row == 0 && col == 4) {
+			return true;
+		}
+		else {
+			return false;
+		}
 	}
 	if (diff_row + diff_col == 1 || diff_row * diff_col == 1) {
 		return true;
