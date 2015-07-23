42082ce01d45d1a309f31547509131d2b0afb773
j437liu <j437liu@student.cs.uwaterloo.ca>
2015-07-23 17:51:50 -0400

    dasfjlasdlkfhalkfh

--- Bishop.cc
+++ Bishop.cc
@@ -6,7 +6,7 @@
 
 bool Bishop::moveCheck(int row, int col, int new_row, int new_col) {
 	if ((row < 0) || (row > 7) || (col < 0) || (col > 7) ||
-		(new_row < 0) || (new_row > 7) || (new_col < 0) || (new_row > 7)) {
+		(new_row < 0) || (new_row > 7) || (new_col < 0) || (new_col > 7)) {
 		return false;
 	}
 	bool tmp;
--- Knight.cc
+++ Knight.cc
@@ -6,7 +6,7 @@
 
 bool Knight::moveCheck(int row, int col, int new_row, int new_col) {
 	if ((row < 0) || (row > 7) || (col < 0) || (col > 7) ||
-		(new_row < 0) || (new_row > 7) || (new_col < 0) || (new_row > 7)) {
+		(new_row < 0) || (new_row > 7) || (new_col < 0) || (new_col > 7)) {
 		return false;
 	}
 	int diff_row = std::abs(row - new_row);
