68dd7717699e229f4fc4e27aa7c02d3762515f43
j437lliu <liujiayi402@gmail.com>
2015-07-26 13:59:36 -0400

    asdiopfyhopasdfhsdo

---
+++ computer2.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "computer.h"
+#include <string>
+#include <vector>
+#include <stdlib.h>
+#include <time.h>
+#include <sstream>
+#include "controller.h"
+
+Computer2::Computer2(Board *game, char team): Controller(game,team) {}
+
+void Computer2::makeMove() {}
