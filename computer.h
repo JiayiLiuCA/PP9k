da229c5265ce899d7fa4d35725da9ed0b192a910
j437lliu <liujiayi402@gmail.com>
2015-07-26 13:01:23 -0400

    update computer

--- computer.h
+++ computer.h
@@ -3,9 +3,39 @@
 #include <string>
 #include "controller.h"
 
-class Computer: public Controller {
+std::string string_convert(int a, int b) {
+	std::string str;
+	int a1 = 8 - a;
+	std::ostringstream ss;
+	ss << a1;
+	str = ss.str();
+	char c1 = 'a' + b;
+	str = c1 + str;
+	return str;
+}
+
+class Computer1: public Controller {
 	public:
 		void makeMove();
-		Computer(Board* game, char team);
+		Computer1(Board* game, char team);
 };
+
+class Computer2: public Controller {
+	public:
+		void makeMove();
+		Computer2(Board* game, char team);
+};
+
+class Computer3: public Controller {
+	public:
+		void makeMove();
+		Computer3(Board* game, char team);
+}
+
+class Computer4: public Controller {
+	public:
+		void makeMove();
+		Computer4(Board* game, char team);
+}
+
 #endif
