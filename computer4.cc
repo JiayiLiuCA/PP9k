437d2ab14047880560a21c17ebed11f3f911be9b
j437lliu <liujiayi402@gmail.com>
2015-07-26 22:39:50 -0400

    asdlfjlsdfjk

---
+++ computer4.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "computer.h"
+#include <string>
+#include <vector>
+#include <stdlib.h>
+#include <time.h>
+#include <sstream>
+#include "pieces.h"
+#include "controller.h"
+#include <cmath>
+
+Computer4::Computer4(Board *game, char team): Controller(game,team) {}
+
+void Computer4::makeMove() {}
