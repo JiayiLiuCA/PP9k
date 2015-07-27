2561fa6f5850baf7a60176eec8266ac5a019315d
zihanchen <zihanchen.ca@gmail.com>
2015-07-27 18:27:18 -0400

    new

--- King.cc
+++ King.cc
@@ -9,17 +9,18 @@ bool King::moveCheck(int row, int col, int new_row, int new_col) {
 	}
 	int diff_row = std::abs(row - new_row);
 	int diff_col = std::abs(col - new_col);
-	if (getStatus() == false && diff_row == 0 && diff_col == 2) {
-		if (getName() == 'P' && row == 7 && col == 4) {
+	if (diff_row == 0 && diff_col == 2){
+		if (getName() == 'K' && row == 7 && col == 4) {
 			return true;
 		}
-		else if (getName() == 'p' && row == 0 && col == 4) {
+		else if (getName() == 'k' && row == 0 && col == 4) {
 			return true;
 		}
 		else {
 			return false;
 		}
 	}
+	std::cout << "not in status false" << std::endl;
 	if (diff_row + diff_col == 1 || diff_row * diff_col == 1) {
 		return true;
 	}
@@ -28,15 +29,15 @@ bool King::moveCheck(int row, int col, int new_row, int new_col) {
 	}
 }
 
-	
+
 void King::setRange() {
-	int dx[] = {0, 1, 0, -1, 1, -1, 1, -1};
-	int dy[] = {1, 0, -1, 0, 1, -1, -1, 1};
+	int dx[] = {0, 1, 0, -1, 1, -1, 1, -1, 2, -2};
+	int dy[] = {1, 0, -1, 0, 1, -1, -1, 1, 0, 0};
 	while(range.size() != 0) {
 		range.pop_back();
 	}
 	std::pair <int, int> tmp;
-	for(int i = 0; i < 8; i ++) {
+	for(int i = 0; i < 10; i ++) {
 		tmp.first = (r + dy[i]);
 		tmp.second = (c + dx[i]);
 		if(moveCheck(r, c, r + dy[i], c + dx[i])) range.push_back(tmp);
