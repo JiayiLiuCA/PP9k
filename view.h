4ca63807e0ec6049d67f413445379d20f6d16bb5
zihanchen <zihanchen.ca@gmail.com>
2015-07-28 11:05:11 -0400

    New veiw

--- view.h
+++ view.h
@@ -3,7 +3,7 @@
 
 class View {
 	public:
-		void virtual notify(int r, int c, char pieces = '-') = 0; //this notifies the display of the changes of the board
+		void virtual notify(int r, int c, char pieces = '_') = 0; //this notifies the display of the changes of the board
 		void virtual print() = 0;
 		virtual ~View() = 0;
 };
